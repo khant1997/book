d4a168039cdb1031070b2eb45ec4c93767f3194c
khant1997 <phonemoekhant.pm2@example.com>
2019-05-23 10:36:07 -0400

    image

--- resources/js/components/Aboutus.vue
+++ resources/js/components/Aboutus.vue
@@ -1,21 +1,21 @@
 <template>
 	<mdb-container class="mb-5">
 		<h2 class="text-center my-5">
-			<strong style="border-bottom: 4px solid #FF7052">About Us</strong>
+			<strong style="border-bottom: 4px solid #FF7052 "><span style="color: rgb(66, 184, 131);">About Us</span></strong>
 		</h2>
 		<mdb-row class="justify-content-center">
 			<mdb-col sm="3">
 				<mdb-card wide>
 					<mdb-view hover cascade>
 						<a href="#!">
-							<mdb-card-image src="https://mdbootstrap.com/img/Photos/Others/men.jpg" alt="Card image cap"></mdb-card-image>
+							<mdb-card-image src="../../images/bg1.jpg" alt="Card image cap"></mdb-card-image>
 							<mdb-mask flex-center waves overlay="white-slight"></mdb-mask>
 						</a>
 					</mdb-view>
 					<mdb-card-body class="text-center" cascade>
-						<mdb-card-title><strong>Billy Cullen</strong></mdb-card-title>
+						<mdb-card-title><strong>Hpone Moe Khant</strong></mdb-card-title>
 						<h5>Web developer</h5>
-						<mdb-card-text>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, ex, recusandae. mdbIconcere modi sunt, quod quibusdam.</mdb-card-text>
+						<mdb-card-text>I want to work as web developer.I am interested in web developing.</mdb-card-text>
 						<mdb-btn tag="a" floating small class="btn-fb"><mdb-icon fab icon="facebook" /></mdb-btn>
 						<mdb-btn tag="a" floating small class="btn-tw"><mdb-icon fab icon="twitter" /></mdb-btn>
 						<mdb-btn tag="a" floating small class="btn-dribbble"><mdb-icon fab icon="dribbble" /></mdb-btn>
